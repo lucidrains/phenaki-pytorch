fd689758c6c1223f8ed80dfaba8b104cee292065
Phil Wang <lucidrains@gmail.com>
2022-10-17 12:29:31 -0700

    attention is all we need

--- phenaki_pytorch/phenaki_pytorch.py
+++ phenaki_pytorch/phenaki_pytorch.py
@@ -4,6 +4,51 @@ from torch import nn, einsum
 
 from einops import rearrange
 
+# helpers
+
+def exists(val):
+    return val is not None
+
+# classes
+
+class Attention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        dim_head = 64,
+        heads = 8
+    ):
+        super().__init__()
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        inner_dim = dim_head * heads
+
+        self.norm = nn.LayerNorm(dim)
+
+        self.to_q = nn.Linear(dim, inner_dim, bias = False)
+        self.to_kv = nn.Linear(dim, inner_dim * 2, bias = False)
+        self.to_out = nn.Linear(inner_dim, dim, bias = False)
+
+    def forward(self, x):
+        x = self.norm(x)
+
+        q, k, v = self.to_q(x), *self.to_kv(x).chunk(2, dim = -1)
+
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), (q, k, v))
+
+        q = q * self.scale
+
+        sim = einsum('b h i d, b h j d -> b h i j', q, k)
+
+        attn = sim.softmax(dim = -1)
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+# main class
+
 class Phenaki(nn.Module):
     def __init__(self):
         super().__init__()
