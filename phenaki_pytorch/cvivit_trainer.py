fix a discriminator gradient issue, thanks to @apoorv2904

phenaki_pytorch/cvivit_trainer.py
@@ -252,6 +252,8 @@ class CViViTTrainer(nn.Module):
         # update discriminator
 
         if exists(self.vae.discr):
+            self.discr_optim.zero_grad()
+
             for _ in range(self.grad_accum_every):
                 img = next(self.dl_iter)
                 img = img.to(device)
@@ -266,7 +268,6 @@ class CViViTTrainer(nn.Module):
                 self.accelerator.clip_grad_norm_(self.vae.discr.parameters(), self.discr_max_grad_norm)
 
             self.discr_optim.step()
-            self.discr_optim.zero_grad()
 
             # log
 